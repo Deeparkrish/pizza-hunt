6daa3e0138be90bc6f85186bca755806ebb664b8
Deeparkrish <kosalai17@gmail.com>
2021-08-28 23:09:22 -0700

    validate models

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,10 +3,14 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
   pizzaName: {
-    type: String
+    type: String,
+    required:true,
+    trim:true
   },
   createdBy: {
-    type: String
+    type: String,
+    required:true,
+    trim:true
   },
   createdAt: {
     type: Date,
@@ -16,10 +20,13 @@ const PizzaSchema = new Schema({
   },
   size: {
     type: String,
-    default: 'Large'
+    required:true,
+    default: 'Large',
+    enum :['Personal','Large','Small','Medium','Extra Large']
   },
   toppings:{
     type :Array,
+    
   },
   comments :[{
     type: Schema.Types.ObjectId,
